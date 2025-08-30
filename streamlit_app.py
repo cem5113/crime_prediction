91ce77a43a4f61f9311e5f4674d142805079df0f
cem5113 <cem5113@hotmail.com>
2025-08-30 11:42:36 +0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import folium
+from streamlit_folium import st_folium
+
+st.set_page_config(layout="wide")
+st.title("Hello Streamlit + Folium")
+m = folium.Map(location=[37.7749, -122.4194], zoom_start=12, tiles="cartodbpositron")
+st_folium(m, height=500)
+st.success("Çalışıyor! Şimdi asıl uygulama kodunu geri koyabilirsiniz.")
