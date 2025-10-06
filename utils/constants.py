ae5e47d2da5ad3ba98f6c1d75aaa88c6e1629e7d
cem5113 <cem5113@hotmail.com>
2025-10-07 01:21:50 +0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,5 @@ SF_TZ_OFFSET = -7
 CRIME_TYPES = ["assault", "burglary", "theft", "robbery", "vandalism"]
 KEY_COL = "geoid"
 CACHE_VERSION = "v2-geo-poisson"
+MODEL_VERSION = "v0.1.0"
+MODEL_LAST_TRAIN = "2025-10-04" 
