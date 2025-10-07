7a7a892216beab0892f8db296e9929b9b7c1459f
cem5113 <cem5113@hotmail.com>
2025-10-07 22:58:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # app.py
+from __future__ import annotations
+
 import os, sys
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
-from __future__ import annotations
+
 import time
 from datetime import datetime, timedelta
-
 import numpy as np
 import pandas as pd
 import streamlit as st
